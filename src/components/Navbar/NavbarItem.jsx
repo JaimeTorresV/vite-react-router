080949ed18c5c62047f9930613b90cf034bd2b0d
Jaime Torres <jaitorres@alumnos.uai.cl>
2023-12-09 00:49:27 -0300

    intento correcion imagenes4

--- src/components/Navbar/NavbarItem.jsx
+++ src/components/Navbar/NavbarItem.jsx
@@ -23,7 +23,7 @@ function NavbarItem() {
       <Container>
         <CDBBox display="flex" alignItems="center">
           <a href="/" className="d-flex align-items-center p-0 text-dark">
-          <img alt="logo" src="/assets/img/Logo.jpg" width="130px" />
+          <img alt="logo" src="/assets/img/Logo.png" width="130px" />
 
           </a>
         </CDBBox>
